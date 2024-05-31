cb2cd1ce5fd2acf1df72f7050e8b9dfb49c98fe5
aloccid-iata <140411726+aloccid-iata@users.noreply.github.com>
2024-05-31 09:22:58 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -44,3 +44,6 @@ async def filter_handler(
         "statusCode": 200,
         "body": filtered_data
     }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
